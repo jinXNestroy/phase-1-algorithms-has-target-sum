d76fb4dcfa70d16df048af049b10e909562cdb49
DanielJoel <danpersie53@gmail.com>
2024-04-16 22:44:49 +0300

    Target sum explanation added

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ console.log(hasTargetSum([3, 8, 12, 4, 11, 7], 10))
 
 /* 
   Write the Big O time complexity of your function here
+  O(n2)
 */
 
 /* 
@@ -29,6 +30,7 @@ console.log(hasTargetSum([3, 8, 12, 4, 11, 7], 10))
 
 /*
   Add written explanation of your solution here
+  The function takes in 2 arguments, an array and another integer. So we iterate over the array with 2 for loops, so that one for loop can point to one index and another one can point to the next one, we compare if the sum is equal to our 2nd argument, if so returns true, if not return false.
 */
 
 // You can run `node index.js` to view these console logs
